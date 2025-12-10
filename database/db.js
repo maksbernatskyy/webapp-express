8acd4d17e520e3e9b8a9d582e5234321166d9fa7
maksbernatskyy <maksbernatskyy@gmail.com>
2025-12-10 12:24:27 +0100

    add dotenv dependency and initialize database connection in db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,16 @@
 const mysql = require('mysql2')
+require('dotenv').config()
 
 const connection = mysql.createConnection({
-    
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.NAME
 })
+
+connection.connect((err) => {
+    if(err) throw err
+    console.log('Connected to MySQL!')
+})
+
+module.exports = connection
