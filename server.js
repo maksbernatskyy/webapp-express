7346d99f91c13a3c45c8dac64d6fe33211b22ae7
maksbernatskyy <maksbernatskyy@gmail.com>
2025-12-10 13:22:43 +0100

    Add error handling middleware for improved error management

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 PORT = 3000
 const moviesRouter = require('./routers/movies')
+const errorsHandler = require('./middlewares/errorsHandler')
 
 app.get('/', (req, res) => {
     res.send('Server of my movies')
@@ -9,6 +10,9 @@ app.get('/', (req, res) => {
 
 // Connect the routers to server
 app.use('/movies', moviesRouter)
+
+// Error 500
+app.use(errorsHandler)
 
 // See in the terminal the number of port
 app.listen(PORT, () => {
