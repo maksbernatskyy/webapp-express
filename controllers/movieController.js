6a47bd8856ff9e222930c5ea8a2c51d6cdd8af3e
maksbernatskyy <maksbernatskyy@gmail.com>
2025-12-10 12:42:01 +0100

    Implement movies router and controller structure

---
+++ controllers/movieController.js
@@ -0,0 +1,6 @@
+// index
+function index(req, res) {
+
+}
+
+module.exports = {index}
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 PORT = 3000
+const moviesRouter = require('./routers/movies')
 
 app.get('/', (req, res) => {
     res.send('Server of my movies')
 })
 
+// Connect the routers to server
+app.use('/movies', moviesRouter)
+
+// See in the terminal the number of port
 app.listen(PORT, () => {
     console.log(`Server listen in port: ${PORT}`)
 })
